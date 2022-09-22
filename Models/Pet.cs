ee0ce2f9513693f6b4c0871d16a416b63efbe66d
Andrew Kane <andrew.kane@gmail.com>
2022-09-22 09:40:19 -0500

    add pet model

--- Models/Pet.cs
+++ Models/Pet.cs
@@ -5,7 +5,40 @@ using System;
 
 namespace pet_hotel
 {
-    public enum PetBreedType {}
-    public enum PetColorType {}
-    public class Pet {}
+    public enum PetBreedType
+    {
+        Shepherd,
+        Poodle,
+        Beagle,
+        Bulldog,
+        Terrier,
+        Boxer,
+        Labrador,
+        Retriever
+    }
+    public enum PetColorType
+    {
+        Black,
+        White,
+        Golden,
+        Tricolor,
+        Spotted
+    }
+    public class Pet
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public string PetBreedType { get; set; }
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public string PetColorType { get; set; }
+
+        public bool checkedIn { get; set; }
+        [ForeignKey("ownedBy")]
+        public int ownedBy { get; set; }
+
+        // public Owner ownedBy {get;set;}
+
+    }
 }
